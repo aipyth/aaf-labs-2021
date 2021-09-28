60fd499eb6d9ab80ad2ec265617d4b61bf82a29e
Ivan <aipyth.func@gmail.com>
2021-09-29 02:21:20 +0300

    Add: storage sheet encoding with gob, add to sheet initialized

--- ruban_fi-91_zhytkevych_fi-91/storage/btree.go
+++ ruban_fi-91_zhytkevych_fi-91/storage/btree.go
@@ -1,1 +1,85 @@
 package storage
+
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+)
+
+const t = 10
+
+type DocId int
+type PosIdx int
+
+type SheetElement struct {
+    Key string
+    Data map[DocId][]PosIdx
+}
+
+type Sheet struct {
+    Keys []*SheetElement
+}
+
+func NewSheet() *Sheet {
+    return &Sheet{
+        Keys: make([]*SheetElement, 0, 2*t-1),
+    }
+}
+
+func (s *Sheet) String() string {
+    return fmt.Sprintf("%v", s.Keys)
+}
+
+func (s *Sheet) Encode() bytes.Buffer {
+    var buf bytes.Buffer
+    encoder := gob.NewEncoder(&buf)
+    err := encoder.Encode(s)
+    if err != nil {
+        log.Println(err)
+    }
+
+    return buf
+}
+
+func (s *Sheet) Add(key string, data map[DocId][]PosIdx) error {
+    if len(s.Keys) == cap(s.Keys) {
+        return errors.New("Sheet full")
+    }
+
+    for i, v := range s.Keys {
+        if v == nil {
+            s.Keys[i] = &SheetElement{
+                Key: key,
+                Data: data,
+            }
+            break
+        }
+
+        cmp := strings.Compare(v.Key, key)
+        switch cmp {
+        case 0:
+            // update data -- merge two maps
+            for k, v := range data {
+                s.Keys[i].Data[k] = v
+            }
+            break
+        case 1:
+            s.Keys = append(
+                s.Keys[:i],
+                &SheetElement{
+                    Key: key,
+                    Data: data,
+                },
+            )
+            s.Keys = append(s.Keys, s.Keys[i:len(s.Keys)-1]...)
+            break
+        case -1:
+            continue
+        }
+    }
+    return nil
+}
+
--- ruban_fi-91_zhytkevych_fi-91/storage/btree_test.go
+++ ruban_fi-91_zhytkevych_fi-91/storage/btree_test.go
@@ -1,1 +1,31 @@
 package storage
+
+import (
+	"bytes"
+	"encoding/gob"
+	"testing"
+)
+
+
+func encode(t *testing.T, i interface{}) bytes.Buffer {
+    var buf bytes.Buffer
+    encoder := gob.NewEncoder(&buf)
+    err := encoder.Encode(i)
+    if err != nil {
+        t.Error(err.Error())
+    }
+    return buf
+}
+
+func TestSheetEncode(t *testing.T) {
+    t.Run("encode empty sheet", func(t *testing.T) {
+        sh := NewSheet()
+        bts := sh.Encode()
+        encoded := encode(t, &Sheet{
+            Keys: make([]*SheetElement, 0),
+        })
+        if bytes.Compare(bts.Bytes(), encoded.Bytes()) != 0 {
+            t.Errorf("Encoded data does not match\n")
+        }
+    })
+}
