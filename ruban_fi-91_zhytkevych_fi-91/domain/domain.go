cf76a1ec6d901a956ebc2c660a7e9b956d78da6d
Hellrasier <denislolipop48@gmail.com>
2021-10-04 09:38:03 +0300

    fixes

--- ruban_fi-91_zhytkevych_fi-91/domain/domain.go
+++ ruban_fi-91_zhytkevych_fi-91/domain/domain.go
@@ -6,143 +6,144 @@ import (
 	"os"
 	"path"
 
-	"github.com/aipyth/aaf-labs-2021/ruban_fi-91_zhytkevych_fi-91/storage"
 	"github.com/aipyth/aaf-labs-2021/ruban_fi-91_zhytkevych_fi-91/indexer"
+	"github.com/aipyth/aaf-labs-2021/ruban_fi-91_zhytkevych_fi-91/storage"
 )
 
 const defaultDomainConfigPath = "./"
 const domainConfigName = "dddb-conf.json"
+
 var defaultDomainConf = &DomainConf{
-    StoragePath: "./",
-    StorageType: "fs",
+	StoragePath: "./",
+	StorageType: "fs",
 }
 
 type Domain struct {
-    CollectionStorage storage.CollectionStorage
-    Indexer indexer.Indexer
+	CollectionStorage storage.CollectionStorage
+	Indexer           indexer.Indexer
 }
 
 type DomainConf struct {
-    StoragePath string `json:"storage_path"`
-    StorageType string `json:"storage_type"`
-    IndexerPath string `json:"indexer_path"`
-    IndexerType string `json:"indexer_type"`
+	StoragePath string `json:"storage_path"`
+	StorageType string `json:"storage_type"`
+	IndexerPath string `json:"indexer_path"`
+	IndexerType string `json:"indexer_type"`
 }
 
 type SearchQuery struct {
-    Keyword string
-    Prefix string
-    N uint
-    KeywordE string
+	Keyword  string
+	Prefix   string
+	N        uint
+	KeywordE string
 }
 
 func NewDomain() *Domain {
-    domain := &Domain{}
-    conf := initDomainConfiguration()
+	domain := &Domain{}
+	conf := initDomainConfiguration()
 
-    var collectionsStorage storage.CollectionStorage
-    var indexr indexer.Indexer
-    var err error
+	var collectionsStorage storage.CollectionStorage
+	var indexr indexer.Indexer
+	var err error
 
-    switch conf.StorageType {
-    case "mem":
-        panic("in memory storage is not implemented")
-    case "fs":
-        collectionsStorage, err = storage.NewCollectionStorageFS(conf.StoragePath)
-        if err != nil {
-            panic(err)
-        }
-    }
+	switch conf.StorageType {
+	case "mem":
+		panic("in memory storage is not implemented")
+	case "fs":
+		collectionsStorage, err = storage.NewCollectionStorageFS(conf.StoragePath)
+		if err != nil {
+			panic(err)
+		}
+	}
 
-    switch conf.IndexerType {
-    case "mem":
-        panic("in memory indexer is not implemented")
-    case "fs":
-        indexr = indexer.NewIndexerBtree(conf.IndexerPath)
-    }
-    
-    domain.CollectionStorage = collectionsStorage
-    domain.Indexer = indexr
+	switch conf.IndexerType {
+	case "mem":
+		panic("in memory indexer is not implemented")
+	case "fs":
+		indexr = indexer.NewIndexerBtree(conf.IndexerPath)
+	}
 
-    return domain
+	domain.CollectionStorage = collectionsStorage
+	domain.Indexer = indexr
+
+	return domain
 }
 
 func initDomainConfiguration() *DomainConf {
-    f, err := os.Open(path.Join(defaultDomainConfigPath, domainConfigName))
-    if err != nil {
-        if err.Error() != "open "  + domainConfigName + ": no such file or directory" {
-           panic(err)
-        } else {
-            return defaultDomainConf
-        }
-    }
+	f, err := os.Open(path.Join(defaultDomainConfigPath, domainConfigName))
+	if err != nil {
+		if err.Error() != "open "+domainConfigName+": no such file or directory" {
+			panic(err)
+		} else {
+			return defaultDomainConf
+		}
+	}
 
-    conf := &DomainConf{}
-    err = json.NewDecoder(f).Decode(conf)
-    if err != nil {
-        panic(err)
-    }
+	conf := &DomainConf{}
+	err = json.NewDecoder(f).Decode(conf)
+	if err != nil {
+		panic(err)
+	}
 
-    return conf
+	return conf
 }
 
 // CreateCollection creates new collection is storage with non empty name
 func (d *Domain) CreateCollection(name string) error {
-    if name == "" {
-        return errors.New("collection name is empty")
-    }
-    return d.CollectionStorage.CreateCollection(name)
+	if name == "" {
+		return errors.New("collection name is empty")
+	}
+	return d.CollectionStorage.CreateCollection(name)
 }
 
 // InsertDocument adds non empty document to storage and indexes it's words
 func (d *Domain) InsertDocument(collectionName string, document string) error {
-    if document == "" {
-        return errors.New("document cannot be empty")
-    }
-    doc, err := d.CollectionStorage.AddDocument(collectionName, []byte(document))
-    if err != nil {
-        return err
-    }
-    
-    err = d.Indexer.IndexDocument(int(doc.Id), []byte(document))
-    if err != nil {
-        return err
-    }
+	if document == "" {
+		return errors.New("document cannot be empty")
+	}
+	doc, err := d.CollectionStorage.AddDocument(collectionName, []byte(document))
+	if err != nil {
+		return err
+	}
 
-    return nil
+	err = d.Indexer.IndexDocument(doc.Id, []byte(document))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (d *Domain) Search(q SearchQuery) []*storage.Document {
-    resp := make([]*storage.Document, 0)
+	resp := make([]*storage.Document, 0)
 
-    switch {
-    case q.KeywordE != "" && q.Keyword != "":
-        idsPos := d.Indexer.GetDocsByKeywords(q.Keyword, q.KeywordE, q.N)
-        for _, v := range idsPos {
-            doc, err := d.CollectionStorage.GetDocumentById(v)
-            if err == nil {
-                resp = append(resp, doc)
-            }
-        }
+	switch {
+	case q.KeywordE != "" && q.Keyword != "":
+		docs, _ := d.Indexer.GetDocsByKeywords(q.Keyword, q.KeywordE, q.N)
+		for _, v := range docs {
+			doc, err := d.CollectionStorage.GetDocumentById(v)
+			if err == nil {
+				resp = append(resp, doc)
+			}
+		}
 
-    case q.Prefix != "":
-        idsPos := d.Indexer.GetDocsByPrefix(q.Prefix)
-        for _, v := range idsPos {
-            doc, err := d.CollectionStorage.GetDocumentById(v)
-            if err == nil {
-                resp = append(resp, doc)
-            }
-        }
+	case q.Prefix != "":
+		docs, _ := d.Indexer.GetDocsByPrefix(q.Prefix)
+		for _, v := range docs {
+			doc, err := d.CollectionStorage.GetDocumentById(v)
+			if err == nil {
+				resp = append(resp, doc)
+			}
+		}
 
-    case q.Keyword != "":
-        idsPos := d.Indexer.GetDocsByKeyword(q.Keyword)
-        for _, v := range idsPos {
-            doc, err := d.CollectionStorage.GetDocumentById(v)
-            if err == nil {
-                resp = append(resp, doc)
-            }
-        }
-    }
+	case q.Keyword != "":
+		docs, _ := d.Indexer.GetDocsByKeyword(q.Keyword)
+		for _, v := range docs {
+			doc, err := d.CollectionStorage.GetDocumentById(v)
+			if err == nil {
+				resp = append(resp, doc)
+			}
+		}
+	}
 
-    return resp
+	return resp
 }
